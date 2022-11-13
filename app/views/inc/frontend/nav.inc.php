bc5ed7ed9bb28a804be1ac49f2da1168a546cc49
Salim Benfarhat <hello@salim.link>
2022-11-13 17:08:19 +0100

    13/11/2022 - Some Changes on the View
    Improved Layout Default HTML Theme + Adding Nav to "inc" path

---
+++ app/views/inc/frontend/nav.inc.php
@@ -0,0 +1,6 @@
+<nav>
+	<a href="<?= URLROOT; ?>" title="The Home page">Home</a>
+	<a href="<?= URLROOT . 'about'; ?>" title="The About page">About</a>
+	<a href="<?= URLROOT . 'auth/login'; ?>" title="Acces to My Space">My Space</a>
+	<a href="<?= URLROOT . 'contact'; ?>" title="The Contact page">Contact</a>
+</nav>
--- app/views/layouts/default.layout.php
+++ app/views/layouts/default.layout.php
@@ -1,13 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?= $variables['meta']['title']; ?></title>
-    <mеtа nаmе="dеѕсrірtіоn" соntеnt="<?= $variables['meta']['description']; ?>">
-  </head>
-  <body>
-    <?= $content; ?>
-  </body>
+<head>
+  <title><?= $variables['meta']['title']; ?></title>
+  <meta charset="UTF-8">
+  <meta name="description" content="<?= $variables['meta']['description']; ?>">
+  <meta name="author" content="Salim Benfarhat">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <base href="https://test.salim.click">
+  <!-- LOADING FAVICON -->
+  <link rel="icon" href="<?= 'public/favicon.ico'; ?>" sizes="any">
+  <link rel="icon" href="<?= 'public/icon.svg'; ?>" type="image/svg+xml">
+  <link rel="apple-touch-icon" href="<?= 'public/icon.png'; ?>">
+  <!-- LOADING CSS FILES -->
+  <link rel="stylesheet" href="public/assets/css/App.css">
+  <!-- LOADING JS FILES -->
+  <script src="<?= 'public/assets/js/App.js'; ?>" async></script>
+</head>
+<body>
+  <h1>SBFRAMEWORK - TEST ENV.</h1>
+  <?= $content; ?>
+</body>
 </html>
