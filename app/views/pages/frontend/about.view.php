431e635c5755d5eecf36587feaec946e6863fbdc
Salim Benfarhat <hello@salim.link>
2022-11-13 17:10:19 +0100

    13/11/2022 - Updating Front Views page
    Home, About and Contact

--- app/views/pages/frontend/about.view.php
+++ app/views/pages/frontend/about.view.php
@@ -1,1 +1,5 @@
-<?= "ABOUT PAGE"; ?>
+<h2>ABOUT PAGE</h2>
+<?php
+// Loading Menu Block View
+require_once(ROOT_DIR.'/app/views/inc/frontend/nav.inc.php');
+?>
--- app/views/pages/frontend/home.view.php
+++ app/views/pages/frontend/home.view.php
@@ -1,21 +1,38 @@
-<?= "HOME PAGE"; ?>
-
+<h2>HOME PAGE</h2>
+<?php
+// Loading Menu Block View
+require_once(ROOT_DIR.'/app/views/inc/frontend/nav.inc.php');
+?>
 <section>
-	<h1>Test Connexion à la BDD</h1>
+	<img width="600" src="<?= 'public/assets/img/banner.png'; ?>" alt="">
+	<h1>Database Connection TEST</h1>
 	<?php 
-	
 	switch(FrontendController::globalVars()['db']::callInstance()->getStatus()) {
-	    case 'connecté' :
-			echo "CONNECTER";
+	    case 'connected' :
+			echo "<pre style='color:green;'>CONNECTION : <b>OK</b></pre>";
 	        break;
-	    case 'non connecté' :
-			echo "NON CONNECTER : " . FrontendController::globalVars()['db']::callInstance()->getMessage();
+	    case 'not connected' :
+			echo "<pre style='color:red;'>CONNECTION : <b>KO</b> => " . FrontendController::globalVars()['db']::callInstance()->getMessage() . "</pre>";
 	        break;
 	}
 	?>
-	<blockquote>
-		<button>Statut : <b class="<?= $class; ?>"><?= FrontendController::globalVars()['db']::callInstance()->getStatus(); ?></b></button>
+	<blockquote> 
+		<?php 
+		$status = FrontendController::globalVars()['db']::callInstance()->getStatus();
+		$status_alert = $status == "connected" ? "green" : "red";
+		?>
+		<button>Status : <b style="color:<?= $status_alert; ?>;"><?= $status; ?></b></button>
 	</blockquote>
-	<blockquote>
-	</blockquote>	
+	<p>Welcome <b><?= $variables['data']['fk_name']; ?></b> !</p>
+	<hr>
+	<h3><?= $variables['data']['msg_info']; ?></h3>
+	<hr>
+	<label for="RANDOM_CHARS_NUMERICAL">NUMERICAL</label>
+	<input type="text" name="RANDOM_CHARS_NUMERICAL" value="<?= $variables['data']['random_num']; ?>" disabled>
+	<label for="RANDOM_CHARS_ALPHABETICAL">ALPHABETICAL</label>
+	<input type="text" name="RANDOM_CHARS_ALPHABETICAL" value="<?= $variables['data']['random_alpha']; ?>" disabled>
+	<label for="RANDOM_CHARS_APLPHANUMERICAL">ALPHANUMERICAL</label>
+	<input type="text" name="RANDOM_CHARS_APLPHANUMERICAL" value="<?= $variables['data']['random_alphanum']; ?>" disabled>
+	<label for="RANDOM_CHARS_FULL">FULL</label>
+	<input type="text" name="RANDOM_CHARS_FULL" value="<?= $variables['data']['random_full']; ?>" disabled>
 </section>	
