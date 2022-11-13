667ea926312e3b5e9e02f61424e23d335281a4b2
Salim Benfarhat <hello@salim.link>
2022-11-13 17:05:24 +0100

    13/11/2022 - Updating Default Controller
    Adding More Libs

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -6,16 +6,20 @@
  */
 
 abstract class Controller {
-
     public static function globalVars() {
         require_once(ROOT_DIR.'/framework/DB.Class.php');
         require_once(ROOT_DIR.'/framework/View.Class.php');
+        require_once(ROOT_DIR.'/libs/vendors/Faker/autoload.php');
+        require_once(ROOT_DIR.'/libs/Alert.Lib.php');
+        require_once(ROOT_DIR.'/libs/Generate.Lib.php');  
         $variables = array(   
             'db' => new DB(),
-            'view' => new View()
+            'view' => new View(),
+            'fk' => Faker\Factory::create(),
+            'alert' => new Alert(),
+            'generate' => new Generate()
         );
         return $variables;
     }
-    
 }
 ?>
